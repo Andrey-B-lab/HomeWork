fdf5fdcf7dddf0c4f52aef0daf2b22cc183e21ce
Andrey Byhalenko <andrey@AB-MacBook-Air.local>
2024-08-29 07:56:05 +0300

    mysql outputs

--- lesson-6/mysql/outputs.tf
+++ lesson-6/mysql/outputs.tf
@@ -1,4 +1,18 @@
-# Ensure the MySQL instance has been fully created and is running
+resource "null_resource" "wait_for_mysql_instance" {
+  provisioner "local-exec" {
+    command = <<EOT
+    echo "Waiting for the MySQL instance to become available..."
+    while [ -z "$(aws ec2 describe-instances --filters "Name=tag:Name,Values=MySQL-Instance" "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].PublicIpAddress" --output text)" ]; do
+      echo "MySQL instance is not ready yet, waiting..."
+      sleep 10
+    done
+    echo "MySQL instance is now available!"
+    EOT
+  }
+  depends_on = [aws_autoscaling_group.mysql]
+}
+
+# Fetch the public IP of the MySQL instance once it's available
 data "aws_instance" "mysql_instance" {
   filter {
     name   = "tag:Name"
@@ -10,8 +24,7 @@ data "aws_instance" "mysql_instance" {
     values = ["running"]
   }
 
-  # Ensure the data source waits for the instance to be available
-  depends_on = [aws_autoscaling_group.mysql]
+  depends_on = [null_resource.wait_for_mysql_instance]
 }
 
 # Output the public IP address of the MySQL instance
